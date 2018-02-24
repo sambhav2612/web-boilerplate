afc507b0dc5e29b442ac2fae1f0a8e3984087784
Sambhav Jain <sambhavjain2612@gmail.com>
2018-02-24 22:05:14 +0530

    update: remove firebase

--- assets/javascript/github.js
+++ assets/javascript/github.js
@@ -1,65 +1,12 @@
 var express = require('express');
-var passport = require('passport');
-var Strategy = require('passport-github').Strategy;
 
-passport.use(new Strategy({
-    clientID: process.env.BOILERPLATE_ID,
-    clientSecret: process.env.BOILERPLATE_SECRET,
-    callbackURL: 'http://localhost:3000/login/github/callback'
-}, function (accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ githubId: profile.id }, function (err, user) {
-        return cb(err, user);
-    });
-}));
+var app = express();
+app.use(express.static('public'));
 
-passport.serializeUser(function (user, cb) {
-    cb(null, user);
-});
-
-passport.deserializeUser(function (obj, cb) {
-    cb(null, obj);
-});
-
-var app = express();    // create a new express app
-app.use(express.static('public'))
-
-app.use(require('morgan')('combined'));
-app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
-
-app.use(passport.initialize());
-app.use(passport.session());
+var port = process.env.PORT || 8888;
 
 // routes
 
-app.get('/', function (req, res) {
-    res.sendFile('index.html', { user: req.user });
+app.listen(port, function () {
+    console.log('Server listening on port ' + port);
 });
-
-app.get('/login', function (req, res) {
-    res.sendFile('login.html');
-});
-
-app.get('/login/github',
-    passport.authenticate('github')
-);
-
-app.get('/login/github/callback',
-    passport.authenticate('github', { failureRedirect: '/login' }),
-    function (req, res) {
-        res.redirect(callbackURL);
-    }
-);
-
-app.get('profile',
-    require('connect-ensure-login').ensureLoggedIn(),
-    function (req, res) {
-        res.sendFile('profile.html', { user: req.user });
-    }
-);
-
-var port = process.env.PORT || 3000;
-
-app.listen(port);
-console.log('Listening at port ' + port);
